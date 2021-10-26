3f4a0d1c3dc00ab1a6798a73978a63dccc9be89b
Ddudooo <igoa.work@gmail.com>
2021-10-26 23:58:36 +0900

    prettier 설정 수정

--- src/creational/factory-method/app.ts
+++ src/creational/factory-method/app.ts
@@ -1,7 +1,7 @@
 import { PointFactory } from './pointFactory'
 import { Point } from './point'
 
-const pointFactory:PointFactory = Point.factory
+const pointFactory: PointFactory = Point.factory
 
 const point = pointFactory.newPolarPoint(5, Math.PI / 2)
 const point2 = pointFactory.newCartesianPoint(5, 6)
